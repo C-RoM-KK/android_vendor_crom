4c010d4dc112bea052b82047464fb2c53af86298
dexmc <dextermcdew@gmail.com>
2014-05-25 11:23:13 -0700

    Remove slim's audio files

--- config/common_full.mk
+++ config/common_full.mk
@@ -2,10 +2,10 @@
 $(call inherit-product, vendor/crom/config/common.mk)
 
 # Bring in all audio files
-include frameworks/base/data/sounds/NewAudio.mk
+#include frameworks/base/data/sounds/NewAudio.mk
 
 # Extra Ringtones
-include frameworks/base/data/sounds/AudioPackageNewWave.mk
+#include frameworks/base/data/sounds/AudioPackageNewWave.mk
 
 # Optional C-RoM packages
 PRODUCT_PACKAGES += \
