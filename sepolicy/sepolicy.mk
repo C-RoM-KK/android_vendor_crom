419f771ee73afef7c7c0a49a5e5cc62f60595be6
aways <aways@c-rom.org>
2014-06-10 04:53:52 +0200

    Update sepolicy

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,6 +1,5 @@
 #
 # This policy configuration will be used by all products that
-# inherit from C-RoM
 #
 
 BOARD_SEPOLICY_DIRS += \
@@ -11,10 +10,7 @@ BOARD_SEPOLICY_UNION += \
     file_contexts \
     fs_use \
     genfs_contexts \
-    seapp_contexts \
     installd.te \
-    netd.te \
-    system.te \
-    ueventd.te \
+    seapp_contexts \
     vold.te \
     mac_permissions.xml
